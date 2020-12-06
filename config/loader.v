789c3c5aa2ae1678a48c96614010f36e0cf2d668
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2020-12-06 18:28:05 +0000

    amv: wbrouw     pts/7        Feb 17 15:02\t(cetus.atnf.csiro.au)

--- config/loader.v
+++ config/loader.v
@@ -7,4 +7,12 @@ pub struct Config {
 pub:
 	repo_storage_path string
 	archive_path      string
-	avatars_path
+	avatars_path      string
+	hostname          string
+}
+
+pub fn read_config(path string) !Config {
+	config_raw := os.read_file(path)!
+
+	return json.decode(Config, config_raw)!
+}

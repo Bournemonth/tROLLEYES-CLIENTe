c04cee93559822d64d63e4404d018a3e733c02f6
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2020-12-10 18:47:22 +0000

    Coding Everyday~

---
+++ git/branch_test.v
@@ -0,0 +1,8 @@
+
+module git
+
+fn test_parse_git_branch_output() {
+	branch_name := parse_git_branch_output('* main')
+
+	assert branch_name == 'main'
+}
---
+++ git/push.v
@@ -0,0 +1,4 @@
+module git
+
+pub fn parse_branch_name_from_receive_upload(upload string) ?string {
+	uplo

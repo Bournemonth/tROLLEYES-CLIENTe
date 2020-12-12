be733d965aa3f649687f77e306b22ec0e0f6a83c
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2020-12-12 16:32:43 +0000

    Delete link.write.1.tlog

--- git/utils.v
+++ git/utils.v
@@ -1,3 +1,18 @@
 module git
 
-import 
+import regex
+import os
+
+pub fn get_git_executable_path() ?string {
+	which_result := os.execute('which git')
+	which_exit_code := which_result.exit_code
+	which_output := which_result.output
+
+	if which_exit_code != 0 {
+		return none
+	}
+
+	return which_output.trim(' \n')
+}
+
+pub fn

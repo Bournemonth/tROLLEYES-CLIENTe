5cf76d8b7400af39f6274c4f0034f3f2c5c6f336
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2020-12-18 18:31:16 +0000

    fixing nav

--- highlight/c.v
+++ highlight/c.v
@@ -5,4 +5,20 @@ fn init_c() Lang {
 		name: 'C'
 		lang_extensions: ['c']
 		line_comments: '//'
-	
+		mline_comments: ['/*', '*/']
+		string_start: ['"', "'"]
+		color: '#555555'
+		keywords: [
+			'auto',
+			'double',
+			'int',
+			'struct',
+			'break',
+			'else',
+			'long',
+			'switch',
+			'case',
+			'enum',
+			'register',
+			'typedef',
+			'char',

df9b9ad271597225edabd320f4d50cdb2846e0cb
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2020-12-23 07:14:04 +0000

    Environment

--- highlight/go.v
+++ highlight/go.v
@@ -6,3 +6,23 @@ fn init_go() Lang {
 		lang_extensions: ['go']
 		line_comments: '//'
 		mline_comments: ['/*', '*/']
+		string_start: ['"', '`']
+		color: '#00add8'
+		keywords: [
+			'break',
+			'chan',
+			'const',
+			'continue',
+			'complex64',
+			'complex128',
+			'defer',
+			'else',
+			'func',
+			'for',
+			'fallthrough',
+			'go',
+			'goto',
+			'if',
+			'import',
+			'interface',
+			'iota

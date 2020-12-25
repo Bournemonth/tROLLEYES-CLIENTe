f92a613a70db75e35edb95f7d13e037bf69035a8
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2020-12-25 10:23:43 +0000

    a few updates

--- highlight/javascript.v
+++ highlight/javascript.v
@@ -2,4 +2,18 @@ module highlight
 
 fn init_js() Lang {
 	return Lang{
-		nam
+		name: 'JavaScript'
+		lang_extensions: ['js', 'mjs', 'jsx']
+		line_comments: '//'
+		mline_comments: ['/*', '*/']
+		string_start: ['"', "'"]
+		color: '#f1e05a'
+		keywords: [
+			'break',
+			'do',
+			'instanceof',
+			'typeof',
+			'case',
+			'else',
+			'new',
+			

9f1042deb445892f2b9be47bf772157969ed8a81
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-01-01 20:56:38 +0000

    libceph: assert both regular and lingering lists in __remove_osd()

--- highlight/python.v
+++ highlight/python.v
@@ -1,1 +1,18 @@
-mod
+module highlight
+
+fn init_py() Lang {
+	return Lang{
+		name: 'Python'
+		lang_extensions: ['py', 'ipynb']
+		line_comments: '#'
+		string_start: ['"', "'", '"""', "'''"]
+		color: '#3572A5'
+		keywords: [
+			'None',
+			'True',
+			'and',
+			'as',
+			'assert',
+			'async',
+			'await',
+			'break'

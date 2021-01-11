587563f064abfd49e34c49582979c3c437ffa1dc
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-01-11 13:08:12 +0000

    Remove tab

--- src/activity_service.v
+++ src/activity_service.v
@@ -14,4 +14,12 @@ fn (app App) add_activity(user_id int, name string) {
 	}
 }
 
-fn (mut app App) find_activities(user_id int) []Acti
+fn (mut app App) find_activities(user_id int) []Activity {
+	return sql app.db {
+		select from Activity where user_id == user_id order by created_at desc
+	}
+}
+
+fn (mut app App) has_activity(user_id int, name string) bool {
+	activity_count := sql app.db {
+		select count from Activity where user_id == user_id && name == 

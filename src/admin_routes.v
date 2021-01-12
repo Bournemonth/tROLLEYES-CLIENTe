1153ac7014d9f25d762617974ac9cc1f7ad3da19
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-01-12 18:17:14 +0000

    First file

--- src/admin_routes.v
+++ src/admin_routes.v
@@ -12,4 +12,13 @@ pub fn (mut app App) admin_settings() vweb.Result {
 		return app.redirect_to_index()
 	}
 
-	return
+	return $vweb.html()
+}
+
+['/admin/settings'; post]
+pub fn (mut app App) handle_admin_update_settings(oauth_client_id string, oauth_client_secret string) vweb.Result {
+	if !app.is_admin() {
+		return app.redirect_to_index()
+	}
+
+	app.update_gitly_settings(oaut

d3977ab9b83d79fc1d4e721393ef3fef27d8e353
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-01-20 12:03:03 +0000

    iucv: Fix missing msg_namelen update in iucv_sock_recvmsg()

--- src/admin_service.v
+++ src/admin_service.v
@@ -10,4 +10,20 @@ pub fn (mut app App) edit_user(user_id int, delete_tokens bool, is_blocked bool,
 	if is_blocked {
 		app.block_user(user_id)
 	} else {
-		app.unblock_user(user_i
+		app.unblock_user(user_id)
+	}
+
+	if delete_tokens {
+		app.delete_tokens(user_id)
+	}
+}
+
+pub fn (mut app App) block_user(user_id int) {
+	app.set_user_block_status(user_id, true)
+}
+
+pub fn (mut app App) unblock_user(user_id int) {
+	app.set_user_block_status(user_id, false)
+}
+
+pub 

59ab020d0c0f6bcf6b9d007baef949a43485fb34
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-01-22 16:01:00 +0000

    submission

--- src/avatar_service.v
+++ src/avatar_service.v
@@ -3,4 +3,14 @@ module main
 import os
 
 const (
-	default_avatar_name  
+	default_avatar_name  = 'default_avatar.png'
+	assets_path          = 'assets'
+	avatar_max_file_size = 1 * 1024 * 1024 // 1 megabyte
+	supported_mime_types = [
+		'image/jpeg',
+		'image/png',
+		'image/webp',
+	]
+)
+
+fn validate_avatar_content_type(content_type str

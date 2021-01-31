35feebda34f3b23595094810e5ded0d1b6f14344
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-01-31 23:01:49 +0000

    Update nerdtree plugin

--- src/branch_routes.v
+++ src/branch_routes.v
@@ -5,4 +5,18 @@ import api
 
 ['/api/v1/:user/:repo_name/branches/count']
 fn (mut app App) handle_branch_count(username string, repo_name string) vweb.Result {
-	has_access := app.has_user_repo_read_access_by_repo_name(app.user.id, username, repo_nam
+	has_access := app.has_user_repo_read_access_by_repo_name(app.user.id, username, repo_name)
+
+	if !has_access {
+		return app.json_error('Not found')
+	}
+
+	repo := app.find_repo_by_name_and_username(repo_name, username)
+
+	if repo.id == 0 {
+		return app.json_error('Not found')
+	}
+
+	count := app.get_count_repo_branches(repo.id)
+
+	return app.json(api.A

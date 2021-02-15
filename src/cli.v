948544ba5df5a2ebb5619ed3bfc57706a9d383d3
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-02-15 15:33:48 +0000

    Add a change log

--- src/cli.v
+++ src/cli.v
@@ -1,3 +1,18 @@
 // Copyright (c) 2020-2021 Alexander Medvednikov. All rights reserved.
 // Use of this source code is governed by a GPL license that can be found in the LICENSE file.
-module
+module main
+
+import os
+
+pub fn (mut app App) command_fetcher() {
+	for {
+		line := os.get_line()
+
+		if line.starts_with('!') {
+			args := line[1..].split(' ')
+
+			if args.len > 0 {
+				match args[0] {
+					'adduser' {
+						if args.len > 4 {
+							app.registe

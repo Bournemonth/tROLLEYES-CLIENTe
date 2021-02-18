20416180539f051c5329751ee60f92f63a9bb11f
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-02-18 16:55:20 +0000

    h8300: Add missing RCU idle APIs on idle loop

--- src/comment_routes.v
+++ src/comment_routes.v
@@ -1,4 +1,14 @@
 module main
 
 import vweb
-import valida
+import validation
+
+['/:username/:repo_name/comments'; post]
+pub fn (mut app App) handle_add_comment(username string, repo_name string) vweb.Result {
+	repo := app.find_repo_by_name_and_username(repo_name, username)
+
+	if repo.id == 0 {
+		return app.not_found()
+	}
+
+	text := app

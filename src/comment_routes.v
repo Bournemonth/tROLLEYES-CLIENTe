650d541b04700a6425e029f253d1f78a67058b68
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-02-19 19:12:37 +0000

    improve docstring

--- src/comment_routes.v
+++ src/comment_routes.v
@@ -11,4 +11,11 @@ pub fn (mut app App) handle_add_comment(username string, repo_name string) vweb.
 		return app.not_found()
 	}
 
-	text := app
+	text := app.form['text']
+	issue_id := app.form['issue_id']
+
+	is_text_empty := validation.is_string_empty(text)
+	is_issue_id_empty := validation.is_string_empty(issue_id)
+
+	if is_text_empty || is_issue_id_empty || !app.logged_in {
+		app.error('Issue comment is not valid')

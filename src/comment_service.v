ae2474e02a86f5d95444474574dff1ef5935d25f
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-02-22 09:51:21 +0000

    Fixed warning.

--- src/comment_service.v
+++ src/comment_service.v
@@ -1,3 +1,15 @@
 module main
 
-import ti
+import time
+
+fn (mut app App) add_issue_comment(author_id int, issue_id int, text string) {
+	comment := Comment{
+		author_id: author_id
+		issue_id: issue_id
+		created_at: int(time.now().unix)
+		text: text
+	}
+
+	sql app.db {
+		insert comment into Comment
+	}

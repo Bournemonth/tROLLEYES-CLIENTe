07a134832f2d561d282cadc7406513a33bc12a03
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-02-23 21:44:58 +0000

    Update events.csv

--- src/commit_service.v
+++ src/commit_service.v
@@ -1,2 +1,15 @@
 // Copyright (c) 2019-2021 Alexander Medvednikov. All rights reserved.
-// Use of this source code is governed by a GPL license that can be found in the LI
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+module main
+
+import time
+
+fn (commit Commit) relative() string {
+	return time.unix(commit.created_at).relative()
+}
+
+fn (commit Commit) get_changes(repo Repo) []Change {
+	git_changes := repo.git('show ${commit.hash}')
+
+	mut change := Change{}
+	mut change

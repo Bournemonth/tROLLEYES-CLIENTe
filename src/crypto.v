a707fb42e483e2d3f20411d42d3731b3e0739e98
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-03-02 20:50:41 +0000

    Rev pub packages

--- src/crypto.v
+++ src/crypto.v
@@ -2,4 +2,15 @@ module main
 
 import rand
 import crypto.rand as crypto_rand
-import encoding.ba
+import encoding.base64
+
+pub const max_safe_unsigned_integer = u32(4_294_967_295)
+
+pub fn set_rand_crypto_safe_seed() {
+	first_seed := generate_crypto_safe_int_u32()
+	second_seed := generate_crypto_safe_int_u32()
+
+	rand.seed([first_seed, second_seed])
+}
+
+pub fn 

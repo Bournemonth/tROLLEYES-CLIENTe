1e4be5746271c018d2b843ff2d039e6f673e4d08
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-03-04 14:05:49 +0000

    finale

--- src/feed_entities.v
+++ src/feed_entities.v
@@ -3,4 +3,12 @@ module main
 import time
 
 // now only for commits
-struct FeedIte
+struct FeedItem {
+	id          int
+	author_name string
+	created_at  time.Time
+	repo_name   string
+	repo_owner  string
+	branch_name string
+	message     string
+}

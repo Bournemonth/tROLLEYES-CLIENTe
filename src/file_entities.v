09c45d3ee5a1295171832d5fa6e35fb1b96ed681
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-03-05 10:43:55 +0000

    fix: test

--- src/file_entities.v
+++ src/file_entities.v
@@ -1,1 +1,8 @@
-// Copyright (c) 2020-2021 Alexander Medved
+// Copyright (c) 2020-2021 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+module main
+
+struct File {
+	id                 int    [primary; sql: serial]
+	repo_id            int    [unique: 'file']
+	name               string 

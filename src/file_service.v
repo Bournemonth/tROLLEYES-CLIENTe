ee135a66f9ebc0bc242f48a99ae24a4bcc9e1dfd
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-03-07 19:17:03 +0000

    README changed

--- src/file_service.v
+++ src/file_service.v
@@ -11,4 +11,19 @@ fn (f File) url() string {
 		return '${file_type}/${f.branch}/${f.name}'
 	}
 
-	return '${file_type}/$
+	return '${file_type}/${f.branch}/${f.parent_path}/${f.name}'
+}
+
+fn (f &File) full_path() string {
+	if f.parent_path == '' {
+		return f.name
+	}
+
+	return f.parent_path + '/' + f.name
+}
+
+fn (f File) pretty_last_time() string {
+	return time.unix(f.last_time).relative()
+}
+
+fn (f File) pretty_siz

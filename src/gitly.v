1d37bed08e38b83e9ef78eaa04531740580d095a
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-04-22 09:00:03 +0000

    New translations translation.json (Afrikaans)

--- src/gitly.v
+++ src/gitly.v
@@ -1,2 +1,16 @@
 // Copyright (c) 2020-2021 Alexander Medvednikov. All rights reserved.
-// Use of this source cod
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+module main
+
+import vweb
+import time
+import os
+import log
+import db.sqlite
+import api
+import config
+
+const (
+	commits_per_page   = 35
+	http_port          = 8080
+	expire_length      = 2

f34de561de22c89c7a5aeb19580e04313753ac84
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-06-08 07:04:06 +0000

    Deploy to heroku

--- src/lang_stats_service.v
+++ src/lang_stats_service.v
@@ -20,3 +20,17 @@ const (
 )
 
 fn (app App) add_lang_stat(lang_stat LangStat) {
+	sql app.db {
+		insert lang_stat into LangStat
+	}
+}
+
+pub fn (l &LangStat) pct_html() vweb.RawHtml {
+	x := f64(l.pct) / 10.0
+	sloc := if l.lines_count < 1000 {
+		l.lines_count.str()
+	} else {
+		(l.lines_count / 1000).str() + 'k'
+	}
+
+	return '<span>${x}%</span> <spa

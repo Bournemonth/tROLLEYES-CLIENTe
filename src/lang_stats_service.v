b0bc547c127d3e1396b0b80e08b6e96ffdcc6c74
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-06-08 08:12:46 +0000

    map update

--- src/lang_stats_service.v
+++ src/lang_stats_service.v
@@ -33,4 +33,13 @@ pub fn (l &LangStat) pct_html() vweb.RawHtml {
 		(l.lines_count / 1000).str() + 'k'
 	}
 
-	return '<span>${x}%</span> <spa
+	return '<span>${x}%</span> <span class=lang-stat-loc>${sloc} loc</span>'
+}
+
+pub fn (app App) find_repo_lang_stats(repo_id int) []LangStat {
+	return sql app.db {
+		select from LangStat where repo_id == repo_id order by pct desc
+	}
+}
+
+fn (app App) remove_repo_l

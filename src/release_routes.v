841bc6af1ce6b9c3343b37de3a78274f59423c25
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-06-12 17:40:50 +0000

    Fixed composer

--- src/release_routes.v
+++ src/release_routes.v
@@ -8,4 +8,10 @@ const (
 )
 
 ['/:username/:repo_name/releases']
-pub fn (mut app App) releases_default(username string, rep
+pub fn (mut app App) releases_default(username string, repo_name string) vweb.Result {
+	return app.releases(username, repo_name, 0)
+}
+
+['/:username/:repo_name/releases/:page']
+pub fn (mut app App) releases(username string, repo_name string, page int) vweb.Result {
+	repo := app.find_repo_by_name_and_usern

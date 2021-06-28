7ec53b2d9dee64dbc9b279e0cf129e68a24d8882
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-06-28 08:45:58 +0000

    Update resourcegroup.py

--- src/release_service.v
+++ src/release_service.v
@@ -11,4 +11,11 @@ pub fn (mut app App) add_release(tag_id int, repo_id int, date time.Time, notes 
 	}
 
 	sql app.db {
-		inse
+		insert release into Release
+	}
+}
+
+pub fn (mut app App) find_repo_releases_as_page(repo_id int, offset int) []Release {
+	// FIXME: 20 -> releases_per_page
+	return sql app.db {
+		select from Release where repo_id == repo_id order by date desc limit 20 offset offs

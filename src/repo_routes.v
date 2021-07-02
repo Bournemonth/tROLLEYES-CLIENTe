18fe30c01b9a3e5a2bc9072356d256496665695c
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-07-02 15:21:04 +0000

    log to file

--- src/repo_routes.v
+++ src/repo_routes.v
@@ -12,4 +12,18 @@ import git
 pub fn (mut app App) user_repos(username string) vweb.Result {
 	exists, user := app.check_username(username)
 
-	if !ex
+	if !exists {
+		return app.not_found()
+	}
+
+	mut repos := app.find_user_public_repos(user.id)
+
+	if user.id == app.user.id {
+		repos = app.find_user_repos(user.id)
+	}
+
+	return $vweb.html()
+}
+
+['/:username/stars']
+pub fn (mut app App) user_stars

760bfdb30e0f2ec3c6676acf99da25ea3ab577fa
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2021-07-06 17:36:22 +0000

    jfs: fix readdir cookie incompatibility with NFSv4

--- src/repo_routes.v
+++ src/repo_routes.v
@@ -44,4 +44,12 @@ pub fn (mut app App) repo_settings(username string, repo_name string) vweb.Resul
 	is_owner := app.check_repo_owner(app.user.username, repo_name)
 
 	if !is_owner {
-		return app.redirect_to_repository(username, repo_name
+		return app.redirect_to_repository(username, repo_name)
+	}
+
+	return $vweb.html()
+}
+
+['/:username/:repo_name/settings'; post]
+pub fn (mut app App) handle_update_repo_settings(username string, repo_name string, webhook_secret string) vweb.Result {
+	repo := app.find_repo_by_name_and_username(repo_name, 

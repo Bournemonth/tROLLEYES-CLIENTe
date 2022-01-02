2903739263a3360d42997ff0595fe6adef07be29
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-01-02 17:33:13 +0000

    Run update-remote.sh on openjdk

--- src/repo_service.v
+++ src/repo_service.v
@@ -1,2 +1,14 @@
 // Copyright (c) 2019-2021 Alexander Medvednikov. All rights reserved.
-// Use of this source co
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+module main
+
+import os
+import time
+import git
+import highlight
+import validation
+
+// log_field_separator is declared as constant in case we need to change it later
+const (
+	max_git_res_size    = 1000
+	log_

5ffd8a71403c3ac5f85998a1d4735ecc759cf834
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-07-15 10:19:15 +0000

    update config.xml

--- src/security_log_service.v
+++ src/security_log_service.v
@@ -1,1 +1,8 @@
-// Copyright 
+// Copyright (c) 2020-2021 Alexander Medvednikov. All rights reserved.
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+module main
+
+fn (mut app App) add_security_log(log SecurityLog) {
+	new_log := SecurityLog{
+		...log
+		kind_id: int(

8e30272146f54b727c5473a2d2c2a746db281f0a
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-07-15 16:47:20 +0000

    Update supplier_quotation.py

--- src/security_log_service.v
+++ src/security_log_service.v
@@ -5,4 +5,16 @@ module main
 fn (mut app App) add_security_log(log SecurityLog) {
 	new_log := SecurityLog{
 		...log
-		kind_id: int(
+		kind_id: int(log.kind)
+		ip: app.ip()
+	}
+
+	sql app.db {
+		insert new_log into SecurityLog
+	}
+}
+
+fn (app &App) get_all_user_security_logs(user_id int) []SecurityLog {
+	mut logs := sql app.db {
+		select from SecurityLog where user_id == user_id order by id desc
+	}

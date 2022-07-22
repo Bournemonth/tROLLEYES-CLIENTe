ddb90f99e6fbefc35d99fd78d7886677806b6f36
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-07-22 07:59:33 +0000

    rank and levels

--- src/settings_service.v
+++ src/settings_service.v
@@ -6,4 +6,15 @@ fn (mut app App) load_settings() {
 	}
 }
 
-fn (mut app App) update_settings(oauth_cli
+fn (mut app App) update_settings(oauth_client_id string, oauth_client_secret string) {
+	old_settings := sql app.db {
+		select from Settings limit 1
+	}
+
+	github_oauth_client_id := if oauth_client_id != '' {
+		oauth_client_id
+	} else {
+		old_settings.oauth_client_id
+	}
+
+	git

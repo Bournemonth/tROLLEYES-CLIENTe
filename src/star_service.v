672f6057612b17ff0213a6076381bccf7324b44b
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-07-27 15:34:54 +0000

    SWG automatic post commit

--- src/star_service.v
+++ src/star_service.v
@@ -1,3 +1,18 @@
 module main
 
-fn (mut app App) add_star(repo_id
+fn (mut app App) add_star(repo_id int, user_id int) {
+	star := Star{
+		repo_id: repo_id
+		user_id: user_id
+	}
+
+	sql app.db {
+		insert star into Star
+	}
+}
+
+fn (mut app App) find_user_starred_repos(user_id int) []Repo {
+	stars := sql app.db {
+		select from Star where user_id == user_id
+	}
+	mut

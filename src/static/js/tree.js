5d1c28fdb5876facb55aa0ac308b05780fefcb19
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-08-27 21:23:08 +0000

    Fix dependencies versions

--- src/static/js/tree.js
+++ src/static/js/tree.js
@@ -1,1 +1,9 @@
-const branchSelectEl = document.querySelector(".branch-s
+const branchSelectEl = document.querySelector(".branch-select");
+branchSelectEl.addEventListener("change", (event) => {
+  window.location.href = TREE_BRANCH_PATH_TEMPLATE + event.target.value;
+});
+
+branchSelectEl.value = BRANCH_NAME;
+
+// Make the entire row clickable
+const fileEls = document.querySelectorAll(".file

9c343d43841d06e8697f901d5cb8a1242415479a
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-08-27 14:55:09 +0000

    toc bump

--- src/static/js/ssh-list.js
+++ src/static/js/ssh-list.js
@@ -1,1 +1,9 @@
-const removeEls = document.querySelec
+const removeEls = document.querySelectorAll(".ssh-key-remove");
+
+[...removeEls].forEach(el => {
+  el.addEventListener("click", () => {
+    const title = el.dataset.title;
+
+    if (confirm("Do you really want to remove \"" + title + "\"?")) {
+      removeSSHKey(el.dataset.username, el.dataset.id)
+        .then(() => 

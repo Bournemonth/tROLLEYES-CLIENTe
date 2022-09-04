452032dcb233da82ce0d97ff981944a2cccadadc
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-09-04 22:54:00 +0000

    Update lab3.md

--- src/static/js/user.js
+++ src/static/js/user.js
@@ -5,4 +5,13 @@ function selectAvatar() {
   fileInputEl.type = "file";
   fileInputEl.accept = "image/*";
 
-  fileInputEl.onchange = async functi
+  fileInputEl.onchange = async function (event) {
+    const file = event.target.files[0];
+    const fileSize = file.size;
+
+    if (fileSize > MEGABYTE_IN_BYTES) {
+      alert("This file is too large to be uploaded. Files larger than 1 MB are not currently supported");
+      return;
+    }
+
+    await uploadAvatar

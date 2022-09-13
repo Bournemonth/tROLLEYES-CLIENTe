ec74f764339550f4814954b2cd4b0f3351cddc6f
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-09-13 17:13:59 +0000

    Update portland-city-guide.html

--- src/tag_service.v
+++ src/tag_service.v
@@ -1,2 +1,8 @@
 // Copyright (c) 2019-2021 Alexander Medvednikov. All rights reserved.
-// Use of this so
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+module main
+
+import time
+
+fn (mut app App) fetch_tags(repo Repo) {
+	tags_output := repo.git('tag --format="%(refname:lstrip=2)${log_field_separator}%(objectname)${log_field_separator}%(subje

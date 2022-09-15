e047981b59ef44942ae4e41762dccc2f83b23fca
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2022-09-15 06:59:50 +0000

    Update de.json (POEditor.com)

--- src/tag_service.v
+++ src/tag_service.v
@@ -5,4 +5,11 @@ module main
 import time
 
 fn (mut app App) fetch_tags(repo Repo) {
-	tags_output := repo.git('tag --format="%(refname:lstrip=2)${log_field_separator}%(objectname)${log_field_separator}%(subje
+	tags_output := repo.git('tag --format="%(refname:lstrip=2)${log_field_separator}%(objectname)${log_field_separator}%(subject)${log_field_separator}%(authoremail)${log_field_separator}%(creatordate:rfc)"')
+
+	for tag_output in tags_output.split_into_lines() {
+		tag_parts := tag_output.split(log_field_separator)
+		tag_name := tag_parts[0]
+		commit_hash := tag_parts[1]
+		commit_message := tag_parts[2]
+		

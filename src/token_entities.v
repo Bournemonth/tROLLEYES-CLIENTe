e36dbe8a6908952dd84a3e4caf33b0bc0694ad71
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2023-01-30 19:52:45 +0000

    Corrected formatting

---
+++ src/token_entities.v
@@ -0,0 +1,9 @@
+
+module main
+
+struct Token {
+	id      int    [primary; sql: serial]
+	user_id int
+	value   string
+	ip      string
+}
---
+++ src/token_service.v
@@ -0,0 +1,2 @@
+// Copyright (c) 2019-2021 Alexander Medvednikov. All rights reserved.
+// Use 

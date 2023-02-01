cdef50a742970bc31f704a22d200794aafaad145
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2023-02-01 20:08:28 +0000

    more configuration

--- src/token_service.v
+++ src/token_service.v
@@ -1,2 +1,11 @@
 // Copyright (c) 2019-2021 Alexander Medvednikov. All rights reserved.
-// Use 
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+module main
+
+import rand
+
+fn (mut app App) add_token(user_id int, ip string) string {
+	mut uuid := rand.uuid_v4()
+
+	token := Token{
+		user_id: user_id

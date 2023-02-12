6610b89ca8e7f4b7bff8671c11c8d9b4d522e4f1
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2023-02-12 19:26:28 +0000

    Restructure.

--- src/watch_service.v
+++ src/watch_service.v
@@ -2,4 +2,20 @@ module main
 
 fn (mut app App) watch_repo(repo_id int, user_id int) {
 	watch := Watch{
-		re
+		repo_id: repo_id
+		user_id: user_id
+	}
+
+	sql app.db {
+		insert watch into Watch
+	}
+}
+
+fn (mut app App) get_count_repo_watchers(repo_id int) int {
+	return sql app.db {
+		select count from Watch where repo_id == repo_id
+	}
+}
+
+fn (mut app App) find_watching_repo_ids(user_id int) []int {
+	watch_list :=

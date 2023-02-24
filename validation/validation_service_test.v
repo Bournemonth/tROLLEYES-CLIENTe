4b9afe42839091873b3b21e39bcd8c1195669f09
Bournemonth <116164190+Bournemonth@users.noreply.github.com>
2023-02-24 06:08:02 +0000

    #66

--- validation/validation_service_test.v
+++ validation/validation_service_test.v
@@ -5,3 +5,11 @@ fn test_is_username_valid() {
 	assert is_username_valid('Gitly')
 	assert is_username_valid('gitly1')
 	assert is_username_valid('git.ly')
+	assert is_username_valid('git3.ly')
+	assert is_username_valid('git3ly_')
+
+	assert is_username_valid('_gitly') == false
+	assert is_username_valid('git-ly') == false
+	assert is_username_valid('1gitly') == false
+	assert is_username_valid('') == false
+	as
